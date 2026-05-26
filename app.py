18bbea8866535c3820903150909b67c13582adb2
Josephinesakerwer <nz_zama@mail.ru>
2026-05-26 16:24:58 +0300

    Change files project

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("Hi")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Main page"
+@app.route("/example")
+def example():
+    k = 10
+    return f"Hi,Example {k}"
+
+if __name__ == "__main__":
+    app.run()
